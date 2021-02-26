c0f0b6bd9be0228db48484bcfe1e7b0b3f4dc8df
Ekene <ekeneanonyai@gmail.com>
2021-02-26 08:44:26 +0000

    updates to docker

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -1,6 +1,6 @@
 \c host_agent;
 CREATE TABLE IF NOT EXISTS PUBLIC.host_info (
-                                                id SERIAL PRIMARY KEY NOT NULL, hostname VARCHAR UNIQUE NOT NULL,
+                                                id SERIAL PRIMARY KEY NOT NULL, hostname VARCHAR UNIQUE gNOT NULL,
                                                 cpu_number INTEGER NOT NULL, cpu_architecture VARCHAR NOT NULL,
                                                 cpu_model VARCHAR NOT NULL, cpu_mhz FLOAT NOT NULL,
                                                 L2_cache INTEGER NOT NULL, total_mem INTEGER NOT NULL,
