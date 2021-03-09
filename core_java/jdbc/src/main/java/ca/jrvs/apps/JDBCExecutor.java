3cca9d510ce17de6b26e72ae4b2c49c6035de975
Ekene <ekeneanonyai@gmail.com>
2021-03-09 09:15:38 +0000

    jdbc changes

--- core_java/jdbc/src/main/java/ca/jrvs/apps/JDBCExecutor.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/JDBCExecutor.java
@@ -1,10 +1,7 @@
 package ca.jrvs.apps;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.List;
 import org.apache.log4j.BasicConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
