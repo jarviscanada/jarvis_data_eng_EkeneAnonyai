a40d563d01c8c0197a4c2b09560dc2de5e9b75e5
Ekene <ekeneanonyai@gmail.com>
2021-03-09 07:43:53 +0000

    jdbc changes

--- core_java/jdbc/src/main/java/ca/jrvs/apps/JDBCExecutor.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/JDBCExecutor.java
@@ -5,12 +5,17 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
+import org.apache.log4j.BasicConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class JDBCExecutor {
 
+    final static Logger logger = LoggerFactory.getLogger(JDBCExecutor.class);
+
     public static void main(String... args){
-        DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost",
-                "hplussport", "postgres", "password");
+        BasicConfigurator.configure();
+        DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost", "hplussport", "postgres", "password");
         try{
             Connection connection = dcm.getConnection();
             CustomerDAO customerDAO = new CustomerDAO(connection);
@@ -20,6 +25,9 @@ public class JDBCExecutor {
                 System.out.println("Page number: " + i);
                 customerDAO.findAllPaged(10, i).forEach(System.out::println);
             }
+            OrderDAO orderDAO = new OrderDAO(connection);
+            Order order = orderDAO.findById(1000);
+            System.out.println(order);
         }catch(SQLException e){
             e.printStackTrace();
         }
